6fbf937870d6170c6dc2b43ebb7a8f194feb73a7
vlad.lopatynets <v.lopatynets@ukr.net>
2022-07-07 22:17:25 +0300

    Initial Commit

---
+++ FillCam/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FillCam
+//
+//  Created by Leyter on 07.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
